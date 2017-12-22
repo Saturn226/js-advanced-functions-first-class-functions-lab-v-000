82f499c8e8f33c60e523bb11f02f212433a677eb
Ashley Muller <serenehopes@gmail.com>
2017-12-22 23:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers(drivers){
-  const firstTwo = [drivers[0], drivers[1]]
-  return firstTwo
+const returnFirstTwoDrivers = (drivers){
+  return  [drivers[0], drivers[1]]
 }
