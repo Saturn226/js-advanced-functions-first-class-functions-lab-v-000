3d4f42bf79d9c04cae115b97d02b87c459433121
Ashley Muller <serenehopes@gmail.com>
2017-12-22 23:29:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const returnFirstTwoDrivers = function (drivers){
 const returnLastTwoDrivers = function (drivers){
   return  [drivers[drivers.length -2], drivers[drivers.length -1]]
 }
+
+const selectingDrivers = function (){
+  
+}
